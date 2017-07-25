b818074ce86f09c06a7887426355293e8cb0ab04
rowanvanekeren <rowanvanekeren@hotmail.com>
2017-07-25 09:25:59 +0200

    settings

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAAGYwbXAYoEBAJkkfXr70d9SMNZADKV5WBg8DGRRCma3VrP8ehZA8OP4GGxmfA7KVlc02yzKlbll0FKsDg4kA61bTmqQNvFN0BfMeogH5vlxoeDNVjATR6p6bNgkIZB3YGczhWmDxKDlfQ9jgLXOKR15oTrz393UBE9QNfirY4nVTSfbXrg',
+    FB_VERIFY_TOKEN: 'new_bot_test_token_for_flux_app_bla',
+    API_AI_CLIENT_ACCESS_TOKEN: '6e7ce2f9c51b4a8bac51386d9a765a0a',
+    FB_APP_SECRET: '18b74870fb9ca3ef4bbbcaf76c1a958b',
+    SERVER_URL: "https://api-ai-test2-rowanvanekeren.c9users.io",
 };
